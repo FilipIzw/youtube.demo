e76f5a7caec3a5f33c42119d2a1ff629a3bf17fb
Sam Sepi0l <rowsi1996@outlook.com>
2020-11-21 15:00:30 +0100

    more changes

--- src/testGitHub.java
+++ src/testGitHub.java
@@ -3,5 +3,7 @@ public class testGitHub
     public static void main(String[] args)
     {
         System.out.println("My first GitHub push");
+
+        System.out.println("Hi, do you want money?");
     }
 }
